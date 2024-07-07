43a4f622ee84404c61b6223a8179f95ee20cae83
YangAi <yangai_21@qq.com>
2024-07-07 17:57:46 +0800

    Update index.ts

--- src/lark/index.ts
+++ src/lark/index.ts
@@ -3,13 +3,20 @@ import * as lark from '@larksuiteoapi/node-sdk';
 let appId: string | null = null;
 let appSecret: string | null = null;
 
+try {
+  
   // @ts-ignore
   appId = application.globalVar.getVar('feishuAppId') || undefined;
   // @ts-ignore
   appSecret = application.globalVar.getVar('feishuAppSecret') || undefined;
+  
+} catch (e) {
+  
+  if (!appId) console.error('Error when getting appId from global variables:', error);
+  if (!appSecret) console.error('Error when getting appSecret from global variables:', error);
+  
+}
 
-if (!appId) console.error('Error when getting appId from global variables:', error);
-if (!appSecret) console.error('Error when getting appSecret from global variables:', error);
 
 let client: any = {};
 
